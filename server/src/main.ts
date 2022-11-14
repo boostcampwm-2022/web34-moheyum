49e6afc6f6d127bfc60d661e55fbf12b19a53a10
namhyo01@naver.com <namhyo01@naver.com>
2022-11-14 15:42:58 +0900

    config파일 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath:[`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true, //전역 사용
+      
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
