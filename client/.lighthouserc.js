e7109a75211cf81a51881267cc652c8a9bcee525
sunmin <polarlsm1231@gmail.com>
2022-11-17 23:32:45 +0900

    test(fe): lighthouse ci 추가 #51

---
+++ client/.lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'yarn build && yarn start', // 서버를 키는 명령어를 통해서도 실행 가능
+      url: ['http://localhost:3000/main'],
+      numberOfRuns: 3,
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: './lhci_reports',
+      reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
+    },
+    assert: {
+      assertions: {
+        'first-contentful-paint': ['warn', { minScore: 0.75 }],
+        'largest-contentful-paint': ['warn', { minScore: 0.75 }],
+      },
+    },
+  },
+};
