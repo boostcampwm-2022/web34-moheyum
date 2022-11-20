ef87570b46901ea05660b9904e9d979675404e09
sunmin <polarlsm1231@gmail.com>
2022-11-20 15:00:54 +0900

    fix(fe):lighthouse js 파일 url 수정 #51

--- client/.lighthouserc.js
+++ client/.lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'yarn start', // 서버를 키는 명령어를 통해서도 실행 가능
-      url: ['http://localhost:3000/main'],
+      url: ['http://localhost:3000/'],
       numberOfRuns: 5,
     },
     upload: {
