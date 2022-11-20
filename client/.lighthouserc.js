9d6c1d3ee673731d0e2b69afcb6b4341ccfc88a9
sunmin <polarlsm1231@gmail.com>
2022-11-21 02:36:40 +0900

    fix(fe): lighthouse 로딩 최대 시간 늘림

--- client/.lighthouserc.js
+++ client/.lighthouserc.js
@@ -5,6 +5,9 @@ module.exports = {
       url: ['http://localhost:3000'],
       numberOfRuns: 5,
     },
+    settings: {
+      maxWaitForLoad: 10000
+    },
     upload: {
       target: 'filesystem',
       outputDir: './lhci_reports',
