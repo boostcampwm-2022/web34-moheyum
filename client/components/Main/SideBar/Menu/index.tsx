ef36342db5e7f7e3e4b28ac63d592d7177393a0f
906bc <57206558+906bc906@users.noreply.github.com>
2022-11-23 14:40:31 +0900

    feat(FE): 사이드바에 유저 프로필 추가 #36

--- client/components/Main/SideBar/Menu/index.tsx
+++ client/components/Main/SideBar/Menu/index.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import Image from 'next/legacy/image';
 import { Wrapper } from './index.style';
 
-export default function Menu({ imgSrc, text }: { imgSrc: string; text: string }) {
+
+export default function Menu({ imgSrc, text, avatar }: { imgSrc: string; text: string, avatar: boolean }) {
+  console.log(text, avatar, imgSrc);
   return (
     <Wrapper>
       <div>
-        <div className="imageBox">
-          <Image src={imgSrc} alt="item" width={25} height={25} priority />
-        </div>
-        <div className="text">{text}</div>
+        {avatar
+        ?
+        <>
+          <div className="avatarBox" style={{backgroundImage: `url(${imgSrc})`}}/>
+          <div className="text">{text}</div>
+        </>
+        :
+        <>
+          <div className="imageBox">
+            <Image src={imgSrc} alt="item" width={25} height={25} priority />
+          </div>
+          <div className="text">{text}</div>
+        </>
+        }
+
       </div>
       <span />
     </Wrapper>
--- client/components/Main/SideBar/index.tsx
+++ client/components/Main/SideBar/index.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useRef, RefObject } from 'react';
 import Link from 'next/link';
+import { useRecoilValue } from 'recoil';
 import Menu from './Menu';
 import Title from './Title';
 import { Dropdown, Setting, SideMenuBox, Wrapper } from './index.style';
+import { authedUser } from '../../../atom';
 
 const menuList = [
-  { routeSrc: '/', imgSrc: '/home.svg', text: '홈' },
-  { routeSrc: '/announce', imgSrc: '/announce.svg', text: '알림' },
-  { routeSrc: '/search', imgSrc: '/search.svg', text: '검색' },
+  { routeSrc: '/', imgSrc: '/home.svg', text: '홈', avatar: false },
+  { routeSrc: '/announce', imgSrc: '/announce.svg', text: '알림', avatar: false },
+  { routeSrc: '/search', imgSrc: '/search.svg', text: '검색', avatar: false },
 ];
 
 export default function SideBar() {
@@ -16,6 +18,8 @@ export default function SideBar() {
   const showSettingdropdown = () => {
     setdropdownState(!dropdownState);
   };
+  const authedUserInfo = useRecoilValue(authedUser);
+  // TODO: 유저 프로필은 주소가 myAccount에서 각자의 프로필 주소로 변경되어야 함, 미구현이라 myAccount로 돌려막기중
   return (
     <Wrapper>
       <Title />
@@ -25,6 +29,11 @@ export default function SideBar() {
             <Menu imgSrc={item.imgSrc} text={item.text} />
           </Link>
         ))}
+        {authedUserInfo.logined &&
+          <Link key="/myAccount" href="/myAccount">
+          <Menu imgSrc={authedUserInfo.profileimg} text={authedUserInfo.nickname} avatar/>
+          </Link>
+        }
       </SideMenuBox>
       {dropdownState && (
         <Dropdown ref={dropdownRef}>
