e12d398ea26bf54beec60cb5118cb3fb1649072b
namhyo01@naver.com <namhyo01@naver.com>
2022-11-24 22:24:54 +0900

    fix(BE): follow, unfollow 없는 id, 출력 result 수정 #77

--- server/src/follow/follow.module.ts
+++ server/src/follow/follow.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Follow, FollowSchema } from 'src/common/database/follow.schema';
 import { FollowRepository } from 'src/common/database/follow.repository';
 import { DatabaseModule } from 'src/common/database/database.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { DatabaseModule } from 'src/common/database/database.module';
     DatabaseModule,
   ],
   controllers: [FollowController],
-  providers: [FollowService, FollowRepository],
+  providers: [FollowService, FollowRepository, UserService],
 })
 export class FollowModule {}
--- server/src/follow/follow.service.ts
+++ server/src/follow/follow.service.ts
@@ -7,41 +7,71 @@ import { UserRepository } from 'src/common/database/user.repository';
 import { FollowRepository } from 'src/common/database/follow.repository';
 import { User } from 'src/common/database/user.schema';
 import { FollowListDto } from './dto/follow-list.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class FollowService {
   constructor(
     private readonly followRepository: FollowRepository,
     private readonly userRepository: UserRepository,
+    private readonly userService: UserService,
   ) {}
 
-  followUser(targetid: string, user: User) {
+  async followUser(targetid: string, user: User) {
+    const data = await this.userService.getUserData(targetid);
+    if (targetid === user.userid)
+      throw new BadRequestException({
+        message: '자기 자신은 팔로우 할 수 없습니다',
+      });
+    if (!data)
+      throw new BadRequestException({
+        message: '존재하지 않는 사용자 입니다',
+      });
+
     const follow = this.followRepository
       .create(targetid, user)
-      .then(() => {
+      .then((res) => {
         this.userRepository.updateFollowingCount({ userid: user.userid }, 1);
         this.userRepository.updateFollowerCount({ userid: targetid }, 1);
+        return res;
       })
       .catch((err) => {
-        console.error(err);
+        console.log(err);
+        if (err.status === 409)
+          throw new BadRequestException({
+            message: '이미 팔로우 된 사용자입니다',
+          });
         throw new BadRequestException({
           message: '팔로우 실패. 요청 값 확인 바람',
         });
       });
     return follow;
   }
-  followCancel(targetid: string, user: User) {
+  async followCancel(targetid: string, user: User) {
+    const data = await this.userService.getUserData(targetid);
+    if (targetid === user.userid)
+      throw new BadRequestException({
+        message: '자기 자신은 팔로우 취소 할 수 없습니다',
+      });
+    if (!data)
+      throw new BadRequestException({
+        message: '존재하지 않는 사용자 입니다',
+      });
     const cancel = this.followRepository
       .delete({
         userid: user.userid,
         targetid,
       })
-      .then(() => {
+      .then((res) => {
         this.userRepository.updateFollowingCount({ userid: user.userid }, -1);
         this.userRepository.updateFollowerCount({ userid: targetid }, -1);
+        return res;
       })
       .catch((err) => {
-        console.error(err);
+        if (err.status === 404)
+          throw new BadRequestException({
+            message: '팔로우가 되어 있지 않는 사용자입니다',
+          });
         throw new BadRequestException({
           message: '팔로우 취소 실패. 요청 값 확인 바람',
         });
