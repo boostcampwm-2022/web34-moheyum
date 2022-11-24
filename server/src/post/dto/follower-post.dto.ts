3f395e8461362fb5753a2c1f0d0dc31cd443b2e0
namhyo01@naver.com <namhyo01@naver.com>
2022-11-24 14:35:56 +0900

    feat(BE): cursor-based pagination #77

--- server/src/post/dto/follower-post.dto.ts
+++ server/src/post/dto/follower-post.dto.ts
@@ -1,6 +1,7 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
-
+import { Transform } from 'class-transformer';
 export class FollowerPostDto {
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @IsOptional()
   limit = 10;
