2b8c7a63872b6a138adb80ab793ced8f9af4d286
sunmin <polarlsm1231@gmail.com>
2022-11-24 17:20:50 +0900

    fix(be): follow 신청, 취소 예외처리 #83

--- server/src/follow/follow.service.ts
+++ server/src/follow/follow.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UserRepository } from 'src/common/database/user.repository';
 import { FollowRepository } from 'src/common/database/follow.repository';
 import { User } from 'src/common/database/user.schema';
@@ -12,18 +16,36 @@ export class FollowService {
   ) {}
 
   followUser(targetid: string, user: User) {
-    const follow = this.followRepository.create(targetid, user);
-    this.userRepository.updateFollowingCount({ userid: user.userid }, 1);
-    this.userRepository.updateFollowerCount({ userid: targetid }, 1);
+    const follow = this.followRepository
+      .create(targetid, user)
+      .then(() => {
+        this.userRepository.updateFollowingCount({ userid: user.userid }, 1);
+        this.userRepository.updateFollowerCount({ userid: targetid }, 1);
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new BadRequestException({
+          message: '팔로우 실패. 요청 값 확인 바람',
+        });
+      });
     return follow;
   }
   followCancel(targetid: string, user: User) {
-    const cancel = this.followRepository.delete({
-      userid: user.userid,
-      targetid,
-    });
-    this.userRepository.updateFollowingCount({ userid: user.userid }, -1);
-    this.userRepository.updateFollowerCount({ userid: targetid }, -1);
+    const cancel = this.followRepository
+      .delete({
+        userid: user.userid,
+        targetid,
+      })
+      .then(() => {
+        this.userRepository.updateFollowingCount({ userid: user.userid }, -1);
+        this.userRepository.updateFollowerCount({ userid: targetid }, -1);
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new BadRequestException({
+          message: '팔로우 취소 실패. 요청 값 확인 바람',
+        });
+      });
     return cancel;
   }
 
