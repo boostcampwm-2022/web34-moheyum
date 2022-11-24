872160f68b5378ca22eff487894857166a283989
906bc <57206558+906bc906@users.noreply.github.com>
2022-11-24 23:03:41 +0900

    chore(FE): console.log 제거

--- client/components/Follow/Following.tsx
+++ client/components/Follow/Following.tsx
@@ -33,7 +33,6 @@ export default function FollowingSection({ userData }: { userData: Props }) {
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && next !== NEXT.END) {
           setNextCursor(next);
-          console.log(next);
         }
       });
       if (node) observer.current.observe(node);
