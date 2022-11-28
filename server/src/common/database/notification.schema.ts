95a64164d0d47a3af21f02e3f8bbf3d28a86c079
906bc <57206558+906bc906@users.noreply.github.com>
2022-11-28 13:44:27 +0900

    feat(BE): notification schema 추가 #133

---
+++ server/src/common/database/notification.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type NotificationDocument = HydratedDocument<Notification>;
+
+@Schema({ versionKey: false, timestamps: true })
+export class Notification {
+  @Prop({
+    required: true,
+    index: true,
+  })
+  userid: string;
+
+  @Prop({
+    required: true,
+  })
+  url: string;
+  
+  @Prop({
+    required: true,
+  })
+  message: string;
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
