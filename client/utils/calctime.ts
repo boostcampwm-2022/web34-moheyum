cf4a656cf35b07aa27cc37825d167f8999290f98
namhyo01@naver.com <namhyo01@naver.com>
2022-11-29 14:42:08 +0900

    fix(FE): forEach => if #76

--- client/utils/calctime.ts
+++ client/utils/calctime.ts
@@ -1,34 +1,12 @@
 export const calcTime = (date: string, type: boolean = false): string => {
   const now: Date = new Date();
   const createdAt: Date = new Date(date);
-  let res: string = '';
-  const times = [
-    {
-      time: false,
-      milliseconds: 1000 * 60 * 60 * 24,
-    },
-    {
-      time: '시간',
-      milliseconds: 1000 * 60 * 60,
-    },
-    {
-      time: '분',
-      milliseconds: 1000 * 60,
-    },
-  ];
   if (type) return createdAt.toLocaleString();
-  try {
-    times.forEach((ms) => {
-      const diff = Math.floor((Number(now) - Number(createdAt)) / ms.milliseconds); // 몇 분, 시간 전 계산
-      if (diff > 0) {
-        if (!ms.time) throw Error();
-        res = `${diff}${ms.time} 전`;
-        throw Error();
-      }
-      return false;
-    });
-  } catch (e) {
-    return res === '' ? createdAt.toLocaleDateString() : res;
-  }
-  return res === '' ? createdAt.toLocaleDateString() : res;
+  const diff = Number(now) - Number(createdAt);
+  let counter = Math.floor(diff / (1000 * 60 * 60 * 24)); // 몇 분, 시간 전 계산
+  if (counter > 0) return createdAt.toLocaleDateString();
+  counter = Math.floor(diff / (1000 * 60 * 60));
+  if (counter > 0) return `${counter}시간 전`;
+
+  return `${Math.floor(diff / (1000 * 60))}분 전`;
 };
