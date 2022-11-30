1365257cb1123c75eea0a8abb582c87ad83b0a65
namhyo01@naver.com <namhyo01@naver.com>
2022-11-30 14:08:12 +0900

    fix(FE): api맞게 수정 #160

--- client/components/Main/Mainsection/index.tsx
+++ client/components/Main/Mainsection/index.tsx
@@ -40,26 +40,25 @@ export default function MainSection() {
         </Link>
         <ArticlesSection>
           {pages.map((item: any, index: number) => {
+            console.log(item);
             if (pages.length === index + 1)
               return (
                 <ArticleCard
-                  author={item.author.author}
-                  key={item.author._id}
-                  id={item.author._id}
-                  description={item.author.description}
-                  date={item.author.createdAt}
-                  comments={item.author.childPosts}
+                  author={item.authorDetail.nickname}
+                  id={item._id}
+                  description={item.description}
+                  date={item.createdAt}
+                  comments={item.childPosts}
                   ref={lastFollowElementRef}
                 />
               );
             return (
               <ArticleCard
-                author={item.author.author}
-                key={item.author._id}
-                id={item.author._id}
-                description={item.author.description}
-                date={item.author.createdAt}
-                comments={item.author.childPosts}
+                author={item.authorDetail.nickname}
+                id={item._id}
+                description={item.description}
+                date={item.createdAt}
+                comments={item.childPosts}
               />
             );
           })}
