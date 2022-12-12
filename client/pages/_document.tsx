c13de858593064e9c7d277ef3c05a28fab75010c
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2022-12-12 14:47:34 +0900

    chore(FE): meta charset 명시, console.log 삭제

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang="ko">
         <Head>
           <meta name="description" content="마크다운을 지원하는 SNS인 모헤윰으로 당신의 생각을 보기 좋게 전달하세요." />
+          <meta charSet="UTF-8" />
           <link rel="icon" href="/favicon.svg" />
         </Head>
         <body>
