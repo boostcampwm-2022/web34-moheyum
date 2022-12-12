35acf6365eff0a9341efca7fe72992a449765539
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2022-12-12 10:41:43 +0900

    fix(FE): 검색어가 없을 때 검색 요청을 보냄

--- client/utils/markdown/rules.ts
+++ client/utils/markdown/rules.ts
@@ -66,7 +66,7 @@ function blockQuote(str: string): string {
       );
       newBlock = newBlock.replace(/(?<=^|\n)\uff1e? {0,3}/g, '');
     }
-    result = result.replace(item, newBlock).replace(/<\/blockquote>\n\n/, '</blockquote>\n');
+    result = result.replace(item, newBlock);
     console.log(result);
   });
 
