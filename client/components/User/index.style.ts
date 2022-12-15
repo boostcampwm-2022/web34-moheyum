52bbae15af6b7147383db15394957bfbe85aa410
sunmin <polarlsm1231@gmail.com>
2022-12-15 11:16:06 +0900

    design(fe): layout 수정 #236

--- client/components/User/index.style.ts
+++ client/components/User/index.style.ts
@@ -1,8 +1,17 @@
 import styled from '@emotion/styled';
 import { mainSectionStyle } from '../../styles/mixin';
+import COLORS from '../../styles/color';
 
 export const Wrapper = styled.div`
   ${mainSectionStyle}
   flex: 1;
   overflow-y: scroll;
+`;
+export const ErrorMessage = styled.span`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  color: ${COLORS.GRAY2};
 `;
